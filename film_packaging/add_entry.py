09b1b5b2368e38ac41b3f9d778fb2c7c42493f56
dekuNukem <dekuNukem@gmail.com>
2024-12-21 18:50:10 +0000

    gitignore

--- film_packaging/add_entry.py
+++ film_packaging/add_entry.py
@@ -1,23 +1,15 @@
 import sys
 
-record_key_list = [
-"md5",
-"",
-]
+record_key_dict = {
+	"film_speed":None,
+	"item_type":["film_box_outside", "film_box_inside", "film_box_instruction_leaflet"],
+	"expiry_date":None,
+	"manufacturer":None,
+	"model_name_full":None, # NOT including brand, including ISO, e.g. Retro 80s, Fujicolor Super HR 200
+	"film_size":["120", "35mm", "APS"],
+	"date_added":None, # unix ts,
+	"item_id":None, # same ID for the same box, can have multiple entries, such as inside, outside, and leaflet
+	"sort_order":None, # smallest number shows up first
+}
 
 
-
-"""
-
-image_md5
-manufactuour
-full_model_name
-film_speed
-expiry_date_YYYY_MM
-item_type: film_box_outside, film_box_inside, film_instruction_leaflet
-uuid
-size: 35mm 120 4x5 APS
-
-
-
-"""
