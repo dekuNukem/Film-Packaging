87ca3b9329d66ae4fec1ffaa2a0e75619d06db6a
dekuNukem <dekuNukem@gmail.com>
2025-01-15 22:44:43 +0000

    does this work

--- push.sh
+++ push.sh
@@ -7,8 +7,8 @@ find . -type f -name "*.DS_Store*" -exec rm -f {} \;
 find . -type f -name "*.eggs*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
 
-bundle config set frozen false
 cp README.md index.md
+bundle config set frozen false
 
 cd film_packaging
 python3 update_stats.py
